d83825dec58b6065fc562b452d0998fcde9949c6
MShamhalov <m@shamhalov.ru>
2023-05-06 23:03:55 +0300

    updated configuration file

--- mathematics/trainer.js
+++ mathematics/trainer.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const prompt = require('../node_modules/prompt-sync')();
-const conf = JSON.parse(fs.readFileSync(__dirname+'/trainer.conf.json', { encoding: 'utf8', flag: 'r' })).config;
+const rawConf = JSON.parse(fs.readFileSync(__dirname+'/trainer.conf.json', {encoding: 'utf8', flag: 'r'}));
+const conf = getCurrentConfig(rawConf);
 
 let i = 0
 let j = false
@@ -10,12 +11,12 @@ while (i < 10) {
     if (j == false) {
         fst = generateRandom(conf.min_number, conf.max_number);
         scd = generateRandom(conf.min_number, conf.max_number);
-        if (conf.operation === '-' && fst < scd) {
+        if (conf.first_number_is_larger && fst < scd) {
             [fst, scd] = [scd, fst]
         }
     }
-    let result = eval(`${fst} ${conf.operation} ${scd}`);
-    const resultCandidate = prompt(`${fst} ${conf.operation} ${scd} = (${result})`);
+    let result = eval(`${fst} ${conf.sign} ${scd}`);
+    const resultCandidate = prompt(`${fst} ${conf.sign} ${scd} = (${result}) `);
     if (result != resultCandidate) {
         console.log('Wrong, try again!')
         j = true
@@ -26,10 +27,29 @@ while (i < 10) {
 }
 console.log(`Calculation time: ${(new Date().getTime()-startTime)/1000} sec`)
 
-function generateRandom(min = 0, max = 100) {
+function generateRandom(min, max) {
     const diff = max - min
     let rand = Math.random()
     rand = Math.floor(rand * diff)
     rand = rand + min
+    
     return rand
+}
+
+function getCurrentConfig(rawJSONConf) {
+    const difficulty = rawJSONConf.config.difficulty;
+    const sign = rawJSONConf.config.operation;
+    let operation;
+
+    if (sign === "+") {
+        operation = "plus";   
+    } else if (sign === "-") {
+        operation = "minus";   
+    } else if (sign === "*") {
+        operation = "multiplication";   
+    } else if (sign === "/") {
+        operation = "division";   
+    }
+
+    return rawJSONConf[difficulty][operation];
 }
